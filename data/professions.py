692d7c6ffa0155675172f99614c865c35ad9bdf1
caspardj23 <caspardejong2003@icloud.com>
2024-03-23 15:31:52 +0100

    Translated template sentences to Dutch

--- data/professions.py
+++ data/professions.py
@@ -8,6 +8,7 @@ from sklearn.model_selection import train_test_split
 MAN = "man"
 WOMAN = "woman"
 
+
 class Dataset(Dataset):
     def __init__(self, data):
         self.data = data
@@ -19,7 +20,8 @@ class Dataset(Dataset):
         X, Xc, y = self.data[index]
         return X, Xc, y
 
-class ProfessionsData():
+
+class ProfessionsData:
     def __init__(self, data_path="./", seed=42):
         super()
         self.data_path = data_path
@@ -30,7 +32,7 @@ class ProfessionsData():
         templates = self.get_template_list()
         male_stereo, female_stereo = self.get_stereo()
         professions = male_stereo + female_stereo
-        
+
         contexts = []
         counter_contexts = []
         labels = []
@@ -46,62 +48,86 @@ class ProfessionsData():
                     labels.append(int())
         data = list(zip(contexts, counter_contexts, labels))
         return data
-    
+
     def get_dataloaders(self, batch_size, shuffle=True, val_split=0.1):
-        train_data, test_data = train_test_split(self.data, test_size=val_split, random_state=self.seed)
+        train_data, test_data = train_test_split(
+            self.data, test_size=val_split, random_state=self.seed
+        )
         train_dataset = Dataset(train_data)
         val_dataset = Dataset(test_data)
         return (
-        DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle), 
-        DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
+            DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle),
+            DataLoader(val_dataset, batch_size=batch_size, shuffle=True),
         )
 
     def get_stereo(self):
         male_stereo = []
         female_stereo = []
-        with open(path.join(self.data_path, "professions_male_stereo.json"), "r") as f:
+        with open(
+            path.join(self.data_path, "professions_male_stereo_nl.json"), "r"
+        ) as f:
             for l in f:
                 for j in eval(l):
                     male_stereo.append(j[0])
-        with open(path.join(self.data_path, "professions_female_stereo.json"), "r") as f:
+        with open(
+            path.join(self.data_path, "professions_female_stereo_nl.json"), "r"
+        ) as f:
             for l in f:
                 for j in eval(l):
                     female_stereo.append(j[0])
 
         return male_stereo, female_stereo
 
-
     def get_profession_list(self, indices=None):
         word_list = []
-        with open(path.join(self.data_path, "professions.json"), "r") as f:
+        with open(path.join(self.data_path, "professions_nl.json"), "r") as f:
             for l in f:
                 for j in eval(l):
                     word_list.append(j[0])
         return word_list
 
     def get_template_list(self, indices=None):
-        '''
-         Get list of all considered templates by Vig et al. 2020
-         A subset from https://arxiv.org/pdf/1807.11714.pdf Lu et al. 2020
-        ''' 
+        """
+        Get list of all considered templates by Vig et al. 2020
+        A subset from https://arxiv.org/pdf/1807.11714.pdf Lu et al. 2020
+        """
+        # templates = [
+        #     "The {} said that",
+        #     "The {} yelled that",
+        #     "The {} whispered that",
+        #     "The {} wanted that",
+        #     "The {} desired that",
+        #     "The {} wished that",
+        #     "The {} ate because",
+        #     "The {} ran because",
+        #     "The {} drove because",
+        #     "The {} slept because",
+        #     "The {} cried because",
+        #     "The {} laughed because",
+        #     "The {} went home because",
+        #     "The {} stayed up because",
+        #     "The {} was fired because",
+        #     "The {} was promoted because",
+        #     "The {} yelled because",
+        # ]
         templates = [
-            "The {} said that",
-            "The {} yelled that",
-            "The {} whispered that",
-            "The {} wanted that",
-            "The {} desired that",
-            "The {} wished that",
-            "The {} ate because",
-            "The {} ran because",
-            "The {} drove because",
-            "The {} slept because",
-            "The {} cried because",
-            "The {} laughed because",
-            "The {} went home because",
-            "The {} stayed up because",
-            "The {} was fired because",
-            "The {} was promoted because",
-            "The {} yelled because",
+            "De {} zei dat",
+            "De {} schreeuwde dat",
+            "De {} fluisterde dat",
+            "De {} wilde dat",
+            "De {} verlangde dat",
+            "De {} wenste dat",
+            "De {} at omdat",
+            "De {} rende omdat",
+            "De {} reed omdat",
+            "De {} sliep omdat",
+            "De {} huilde omdat",
+            "De {} lachte omdat",
+            "De {} ging naar huis omdat",
+            "De {} bleef wakker omdat",
+            "De {} was ontslagen omdat",
+            "De {} was gepromoveerd omdat",
+            "De {} schreeuwde omdat",
         ]
         if indices:
             subset_templates = [templates[i - 1] for i in indices]
@@ -109,17 +135,20 @@ class ProfessionsData():
             return subset_templates
         return templates
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     data = ProfessionsData(seed=1)
-    train_dataloader, val_dataloader = data.get_dataloaders(batch_size=1, shuffle=True, val_split=0.00001)  
+    train_dataloader, val_dataloader = data.get_dataloaders(
+        batch_size=1, shuffle=True, val_split=0.00001
+    )
     m, f = data.get_stereo()
     print(len(m))
     print(len(f))
     print(len(train_dataloader.dataset.data))
     print(len(val_dataloader.dataset.data))
     for batch in train_dataloader:
-       X, Xc, y = batch
-       print(X[0]+ " he")
-       print(X[0]+ " she")
-       print(y[0].item())
-       break
+        X, Xc, y = batch
+        print(X[0] + " he")
+        print(X[0] + " she")
+        print(y[0].item())
+        break
