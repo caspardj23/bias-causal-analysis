529d16d9fd2f55792076a3e737497c3ec9330886
caspardj23 <caspardejong2003@icloud.com>
2024-05-08 11:29:58 +0200

    stereoset formatted

--- evaluation/stereoset.py
+++ evaluation/stereoset.py
@@ -8,33 +8,41 @@ from data.stereo import StereoSet
 
 
 def eval_stereoset(model, tokenizer, data, bias, device):
-    evaluator = BiasEvaluator(model=model, tokenizer=tokenizer, data=data, bias=bias, device=device)
+    evaluator = BiasEvaluator(
+        model=model, tokenizer=tokenizer, data=data, bias=bias, device=device
+    )
     intrasentence_bias = evaluator.evaluate_intrasentence()
     bias = {}
-    bias['intrasentence'] = intrasentence_bias
+    bias["intrasentence"] = intrasentence_bias
     score_evaluator = ScoreEvaluator(data, bias)
     results = score_evaluator.get_overall_results()
-    return results['intrasentence']
+    return results["intrasentence"]
 
 
 UNCONDITIONAL_START_TOKEN = "<|endoftext|>"
 
-class BiasEvaluator():
+
+class BiasEvaluator:
     def __init__(self, model, tokenizer, data, bias, device):
         self.dataloader = StereoSet(data, bias)
         self.tokenizer = tokenizer
         self.model = model
         self.device = device
-       
+
     def evaluate_intrasentence(self):
         model = self.model
-        model.to(self.device)   
+        model.to(self.device)
         model.eval()
 
-        start_token = torch.tensor(self.tokenizer.encode(UNCONDITIONAL_START_TOKEN)).to(self.device).unsqueeze(0)
+        start_token = (
+            torch.tensor(self.tokenizer.encode(UNCONDITIONAL_START_TOKEN))
+            .to(self.device)
+            .unsqueeze(0)
+        )
         initial_token_probabilities = model(start_token)
         initial_token_probabilities = torch.softmax(
-            initial_token_probabilities[0], dim=-1)
+            initial_token_probabilities[0], dim=-1
+        )
 
         # ensure that our batch size is 1, and that our initial token isn't split into subwords.
         assert initial_token_probabilities.shape[0] == 1
@@ -47,28 +55,29 @@ class BiasEvaluator():
                 probabilities = {}
                 tokens = self.tokenizer.encode(sentence.sentence)
                 joint_sentence_probability = [
-                    initial_token_probabilities[0, 0, tokens[0]].item()]
-                tokens_tensor = torch.tensor(
-                    tokens).to(self.device).unsqueeze(0)
+                    initial_token_probabilities[0, 0, tokens[0]].item()
+                ]
+                tokens_tensor = torch.tensor(tokens).to(self.device).unsqueeze(0)
                 output = torch.softmax(model(tokens_tensor)[0], dim=-1)
                 for idx in range(1, len(tokens)):
                     joint_sentence_probability.append(
-                        output[0, idx-1, tokens[idx]].item())
+                        output[0, idx - 1, tokens[idx]].item()
+                    )
 
                 # ensure that we have a probability on every token
                 assert len(tokens) == len(joint_sentence_probability)
 
-                score = np.sum([np.log2(i)
-                                for i in joint_sentence_probability])
+                score = np.sum([np.log2(i) for i in joint_sentence_probability])
                 score /= len(joint_sentence_probability)
                 score = np.power(2, score)
 
-                probabilities['id'] = sentence.ID
-                probabilities['score'] = score
+                probabilities["id"] = sentence.ID
+                probabilities["score"] = score
 
                 predictions.append(probabilities)
 
         return predictions
+
 
 class ScoreEvaluator(object):
     def __init__(self, gold_file_path, predictions):
@@ -86,20 +95,19 @@ class ScoreEvaluator(object):
             for sentence in example.sentences:
                 self.id2term[sentence.ID] = example.target
                 self.id2gold[sentence.ID] = sentence.gold_label
-                self.example2sent[(
-                    example.ID, sentence.gold_label)] = sentence.ID
-                self.domain2example['intrasentence'][example.bias_type].append(
-                    example)
+                self.example2sent[(example.ID, sentence.gold_label)] = sentence.ID
+                self.domain2example["intrasentence"][example.bias_type].append(example)
 
-        for sent in self.predictions.get('intrasentence', []):
-            self.id2score[sent['id']] = sent['score']
+        for sent in self.predictions.get("intrasentence", []):
+            self.id2score[sent["id"]] = sent["score"]
 
         results = defaultdict(lambda: {})
 
-        for split in ['intrasentence']:
-            for domain in ['gender']:
+        for split in ["intrasentence"]:
+            for domain in ["gender"]:
                 results[split][domain] = self.evaluate(
-                    self.domain2example[split][domain])
+                    self.domain2example[split][domain]
+                )
 
         self.results = results
 
@@ -121,20 +129,20 @@ class ScoreEvaluator(object):
             # assert self.id2score[unrelated_id] != self.id2score[anti_id]
 
             # check pro vs anti
-            if (self.id2score[pro_id] > self.id2score[anti_id]):
+            if self.id2score[pro_id] > self.id2score[anti_id]:
                 per_term_counts[example.target]["pro"] += 1.0
             else:
                 per_term_counts[example.target]["anti"] += 1.0
 
             # check pro vs unrelated
-            if (self.id2score[pro_id] > self.id2score[unrelated_id]):
+            if self.id2score[pro_id] > self.id2score[unrelated_id]:
                 per_term_counts[example.target]["related"] += 1.0
 
             # check anti vs unrelatd
-            if (self.id2score[anti_id] > self.id2score[unrelated_id]):
+            if self.id2score[anti_id] > self.id2score[unrelated_id]:
                 per_term_counts[example.target]["related"] += 1.0
 
-            per_term_counts[example.target]['total'] += 1.0
+            per_term_counts[example.target]["total"] += 1.0
 
         return per_term_counts
 
@@ -145,9 +153,9 @@ class ScoreEvaluator(object):
         total = 0
 
         for term, scores in counts.items():
-            total += scores['total']
-            ss_score = 100.0 * (scores['pro'] / scores['total'])
-            lm_score = (scores['related'] / (scores['total'] * 2.0)) * 100.0
+            total += scores["total"]
+            ss_score = 100.0 * (scores["pro"] / scores["total"])
+            lm_score = (scores["related"] / (scores["total"] * 2.0)) * 100.0
 
             lm_scores.append(lm_score)
             ss_scores.append(ss_score)
@@ -158,4 +166,9 @@ class ScoreEvaluator(object):
         ss_score = np.mean(ss_scores)
         micro_icat = np.mean(micro_icat_scores)
         macro_icat = lm_score * (min(ss_score, 100 - ss_score) / 50.0)
-        return {"Count": total, "LM Score": lm_score, "SS Score": ss_score, "ICAT Score": macro_icat}
+        return {
+            "Count": total,
+            "LM Score": lm_score,
+            "SS Score": ss_score,
+            "ICAT Score": macro_icat,
+        }
