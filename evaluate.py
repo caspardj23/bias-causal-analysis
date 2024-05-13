e7afb60acb51b0048c479a30e18513fe1acd7e4d
caspardj23 <caspardejong2003@icloud.com>
2024-05-13 17:10:21 +0200

    test

--- evaluate.py
+++ evaluate.py
@@ -143,19 +143,19 @@ def evaluate(
 
 def save_results(results, model_name, seed):
     with open(
-        f"content/drive/My Drive/Mitigation_data/GroNLP/Evaluate/{model_name}_{seed}.json",
+        f"My Drive/Mitigation_data/GroNLP/Evaluate{model_name}_{seed}.json",
         "w",
     ) as f:
         json.dump(results, f)
     print(
         "Succesfully saved results at: ",
-        f"content/drive/My Drive/Mitigation_data/GroNLP/Evaluate/{model_name}_{seed}.json",
+        f"My Drive/Mitigation_data/GroNLP/Evaluate/{model_name}_{seed}.json",
     )
 
 
 def load_results(model_name, seed):
     with open(
-        f"content/drive/My Drive/Mitigation_data/GroNLP/Evaluate/{model_name}_{seed}.json",
+        f"My Drive/Mitigation_data/GroNLP/Evaluate/{model_name}_{seed}.json",
         "r",
     ) as f:
         results = json.load(f)
