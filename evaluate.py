f8c4f11331b366927607606960be841018a29c25
caspardj23 <caspardejong2003@icloud.com>
2024-05-15 17:09:54 +0200

    eval seed 12

--- evaluate.py
+++ evaluate.py
@@ -196,7 +196,7 @@ def evaluate_save_results():
     checkpoint_path = Path("/content/drive/My Drive/Mitigation_data/GroNLP")
     list_of_files = list(checkpoint_path.glob("*.pt"))
     for f in list_of_files:
-        if "_seed_11_" in f.stem:
+        if "_seed_12_" in f.stem:
             name = f.stem
             # Find other split
             s = name.split(".pt")
