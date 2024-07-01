57a634942729f01018b9bfef31dbd8e7f4fbe35e
caspardj23 <caspardejong2003@icloud.com>
2024-07-01 12:41:12 +0200

    remove prints

--- mitigate.py
+++ mitigate.py
@@ -72,7 +72,7 @@ def fine_tune(config: MitigationConfig):
     filename = f"{config.model.name}_{config.model.components}_seed_{config.seed}"
     if filename.startswith("yhavinga/"):
         filename = filename[len("yhavinga/") :]
-    print("Model Config: ", model.config)
+    # print("Model Config: ", model.config)
 
     trainer = pl.Trainer(
         accumulate_grad_batches=4,
